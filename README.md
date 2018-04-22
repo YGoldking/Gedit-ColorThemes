2c2f8fd1a064cd022cf6e722a2fac9756da9d012
Yang <354096994@qq.com>
2018-04-22 08:56:12 +0800

    ...

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Gedit-ColorThemes
+
+## gedit 配色方案
+
+```
+$ git clone https://github.com/YGoldking/Gedit-ColorThemes.git
+$ cd Gedit-ColorThemes
+$ ./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo cp --no-clobber ./usr/share/gtksourceview-2.0/styles/*.xml /usr/share/gtksourceview-2.0/styles
+sudo cp --no-clobber ./usr/share/gtksourceview-3.0/styles/*.xml /usr/share/gtksourceview-3.0/styles
